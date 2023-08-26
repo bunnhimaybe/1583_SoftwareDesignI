845fef1bfbf1120fd6ba9f6dc48a1d824ccaa65c
lapham <lisanapham@gmail.com>
2023-08-25 22:26:22 -0500

    corrections

--- hw1/src/Greeting.java
+++ hw1/src/Greeting.java
@@ -9,12 +9,10 @@ public class Greeting{ // class declaration
 	public static void main(String[] args){ // begins w/ main method 
 		Scanner input = new Scanner(System.in);	// creates Scanner obj
 		System.out.println("What is your name?"); // displays prompt for input
-		String name = input.next(); // saves input data into name var
+		String name = input.nextLine(); // saves input data into name var
 		System.out.println("What salutation would you like me to use?"); // prompt
-		String salutation = input.next(); // saves input into salutation
+		String salutation = input.nextLine(); // saves input into salutation
 		System.out.printf("\n%s%s%s%s\n", salutation, " ", name, ", it is nice to meet you! Goodbye!"); // greets user
-		/* the homework instructions say to put "Hello <salutation> <name>" 
-		but Hello <salutation would be redundant. 
-		pls don't subtract points */
+		input.close(); // frees memory
 	} // end method main
 } // end class Greeting
