5a6e5c257d8c9720cbe4ff8e49a7eb5de500b324
lapham <lisanapham@gmail.com>
2023-10-02 13:06:03 -0500

    SumOfDigits and README

---
+++ hw4/README.md
@@ -0,0 +1,25 @@
+## SumOfDigits.java
+
+## Description
+
+This program calculates the sum of all individual digits in a positive integer given by the user. If the sum contains multiple digits, the program will continue to calculate the sum and output a single digit. 
+
+## Execution
+
+To run this program, navigate to the directory with <cd "PATH"/lapham_1583/hw2/src>.
+Compile the program with the command <javac SumOfDigits.java>.
+Run the program with the command <java SumOfDigits>.
+
+## Sample
+
+57      // input
+7       // output
+
+538     // input
+7       // output
+
+7       // input
+7       // output
+
+12345   // input
+6       // output
---
+++ hw4/src/SumOfDigits.java
@@ -0,0 +1,31 @@
+/*  Sum of Digits
+    
+    Calculates the sum of individual digits in a given positive integer. 
+    Continues to calculate the sum to get a single digit. 
+
+    Homework 4
+    Nhi Pham
+    due 10/3 Tue
+*/
+
+import java.util.Scanner;
+
+public class SumOfDigits{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int value = Math.abs(input.nextInt());  // takes absoloute value of input
+        int temp = value;                       // temp contains working number, beginning with value input
+        
+        while (temp != 0){                      // loops until all digits have been added
+            sum += temp % 10;                       // last digit of working number added to sum
+            temp /= 10;                             // last digit is removed from working number
+            if (temp == 0 && sum > 10) {                // if sum of digits in working number is more than one digit, 
+                temp = sum;                             // set sum as nworking number
+                sum = 0;                                // reset sum
+            }
+        }
+        System.out.println(sum);                // output final single-digit sum
+        input.close();                          // that's it
+    }
+}
